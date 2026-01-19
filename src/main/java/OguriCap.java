3baa13cdb7787caa2e58a8db6b1ea9e5d9cfe611
Cody Loo Yee <codyly3005@gmail.com>
2026-01-19 12:17:24 +0800

    Level-0: Rename chatbot and add greeting

---
+++ src/main/java/OguriCap.java
@@ -0,0 +1,15 @@
+public class OguriCap {
+    public static void main(String[] args) {
+
+        String chatBotName = "Oguri Cap";
+        String byeMsg = "Bye. Hope to see you again soon!";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println("Hello! I'm " + chatBotName);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println(byeMsg);
+
+    }
+}
