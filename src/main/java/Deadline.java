66f33a12885585db125076aa27d77b1e9115265c
Cody Loo Yee <codyly3005@gmail.com>
2026-01-26 23:56:22 +0800

    Level 8: Dates and Times
    Chatbot now understands date and times by storing dates as local date objects.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,32 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    protected String by;
+    private LocalDate byDate;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by) throws DukeException {
         super(description);
-        this.by = by;
+        try {
+            this.byDate = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Invalid date format! Use yyyy-MM-dd");
+        }
+    }
+
+    public LocalDate getByDate() {
+        return byDate;
     }
 
     @Override
     public String toFileString() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + byDate.toString();
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() +
+                " (by: " + byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,22 +1,41 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    private LocalDate fromDate;
+    private LocalDate toDate;
 
-    public Event(String description, String from, String to) {
+    public Event(String description, String from, String to) throws DukeException{
         super(description);
-        this.from = from;
-        this.to = to;
+        try {
+            this.fromDate = LocalDate.parse(from);
+            this.toDate = LocalDate.parse(to);
+        } catch (DateTimeParseException e) {
+            throw new DukeException("Invalid date format! Use yyyy-MM-dd for /from and /to");
+        }
+    }
+
+    public LocalDate getFromDate() {
+        return fromDate;
+    }
+
+    public LocalDate getToDate() {
+        return toDate;
     }
 
     @Override
     public String toFileString() {
-        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
+        return "E | " + (isDone ? "1" : "0") + " | " + description +
+                " | " + fromDate.toString() + " | " + toDate.toString();
     }
 
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " +
+                fromDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) +
+                " to: " + toDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
