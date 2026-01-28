1e573caac0e1a864b1846ca5e1201afe228e5d57
Cody Loo Yee <codyly3005@gmail.com>
2026-01-28 22:31:44 +0800

    Level 9. Find
    Give users a way to find a task by searching for a keyword in the task description.
    Implement a 'find' command that allows users to search for tasks containing a specific keyword in their descriptions.
    
    Level 9. Find

--- src/main/java/oguricap/Parser.java
+++ src/main/java/oguricap/Parser.java
@@ -1,7 +1,18 @@
 package oguricap;
 
-import oguricap.command.*;
-import oguricap.task.*;
+import oguricap.command.Command;
+import oguricap.command.AddCommand;
+import oguricap.command.DeleteCommand;
+import oguricap.command.ExitCommand;
+import oguricap.command.ListCommand;
+import oguricap.command.MarkCommand;
+import oguricap.command.UnmarkCommand;
+import oguricap.command.FindCommand;
+
+import oguricap.task.Deadline;
+import oguricap.task.Event;
+import oguricap.task.Todo;
+
 import oguricap.exception.DukeException;
 
 public class Parser {
@@ -37,6 +48,9 @@ public class Parser {
             String[] toParts = evParts[1].split("/to", 2);
             if (toParts.length < 2) throw new DukeException("Event must have /to");
             return new AddCommand(new Event(evParts[0].trim(), toParts[0].trim(), toParts[1].trim()));
+        case "find":
+            checkArgument(parts, "find");
+            return new FindCommand(parts[1]);
         default:
             throw new DukeException("Hmm...Unknown command: " + cmd);
         }
--- src/main/java/oguricap/command/FindCommand.java
+++ src/main/java/oguricap/command/FindCommand.java
@@ -1,4 +1,29 @@
 package oguricap.command;
 
-public class FindCommand {
+import oguricap.TaskList;
+import oguricap.Ui;
+import oguricap.Storage;
+import oguricap.exception.DukeException;
+import oguricap.task.Task;
+
+import java.util.ArrayList;
+
+public class FindCommand extends Command {
+
+    private final String keyword;
+
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
+        ArrayList<Task> matches = tasks.find(keyword);
+        ui.showFoundTasks(matches);
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
